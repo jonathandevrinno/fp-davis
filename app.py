0e9670b879179ed7353afbfc7545f95db071553e
jonathandevrinno <142668097+jonathandevrinno@users.noreply.github.com>
2024-06-11 17:22:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ st.set_page_config(page_title="FINAL PROJECT VISUALISASI DATA",
 
 # Set the page title
 st.title("Final Project Visualisasi Data")
+
 # Owner Name
 st.header("Ownership:")
-st.markdown("This task is created by: \n[Jonathan Devrinno](https://www.linkedin.com/in/jonathandevrinno/) \nNPM:21082010204.")
+st.markdown("This task is created by: \n[Jonathan Devrinno](https://www.linkedin.com/in/jonathandevrinno/) (21082010204)")
 
 # Sidebar menu
 option = st.sidebar.selectbox("Select a feature", ["AdventureWorks", "IMDb Scrapping"])
